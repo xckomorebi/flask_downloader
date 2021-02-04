a1fff14666c08c0f4a13d2ac750a5347c12faf71
ubuntu <ubuntu@localhost.localdomain>
2021-02-04 13:40:57 +0800

    update

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
-#! /home/xuchen/flask_downloader/venv/bin/python
+#! /home/ubuntu/flask_downloader/venv/bin/python
+
 
 from flask import Flask, send_from_directory
 import os
 app = Flask(__name__)
 
 app.config['UPLOAD_FOLDER'] = 'uploads'
+
+@app.route('/')
+def index():
+   return """<html>
+    <title>xc简陋的主页</title>
+    <body>
+        <h1>小机灵鬼们你们好</h1>
+        <h1>测试海外服务器需不需要备案</h1>
+        <body>别问为啥这么简陋，问就是懒</body>
+    </body>
+</html>"""
 
 @app.route('/uploads/<path:filename>', methods=['GET', 'POST'])
 def download(filename):
